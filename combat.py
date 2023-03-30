631ce5f7ca738fb1de2e6681a6215c6fb8b96e0e
Florin <florin1357@gmail.com>
2023-03-30 16:11:11 +0200

    0.0.6

--- combat.py
+++ combat.py
@@ -6,12 +6,13 @@ from time import sleep
 
 def combat(player, zombie):
     while player.health > 0 and zombie.health > 0:
-        print("What do you want to do? [1] Shoot  [2] Hit\n [3] Inventory  [4] Run")
+        print(
+            "What do you want to do? [1] Shoot  [2] Hit\n [3] Inventory  [4] Run")
         choice = input()
         if choice == "1" and player.shoot(zombie.name) == True:
             if player.health > 0:
+                player.calc_damage()
                 player.shoot(zombie.name)
-                zombie.health - player.damage
                 sleep(1)
                 zombie.defend(player)
                 sleep(1)
@@ -23,7 +24,6 @@ def combat(player, zombie):
         elif choice == "2" and player.hit(zombie.name) == True:
             if player.health > 0:
                 player.hit(zombie.name)
-                zombie.health - player.damage
                 sleep(1)
                 zombie.defend(player)
                 sleep(1)
@@ -33,12 +33,13 @@ def combat(player, zombie):
                 player.take_damage(zombie.damage)
                 sleep(1)
         elif choice == "3":
-                player.check_inventory()
+            player.check_inventory()
         elif choice == "4":
-             print(run())
-             break
+            print(run())
+            break
         else:
             continue
+
 
 def run():
     return f"You ran away from the fight... Coward!"
--- playable_chars.py
+++ playable_chars.py
@@ -7,33 +7,35 @@ class Character:
         self.level = 1
         self.inventory = []
         self.weapon = []
+        
 
     def get_info(self):
-       print(f"Your character {self.name}'s current stats:\n current Level: {self.level} Health: {self.health}\n Damage: {self.damage}\n Armor: {self.armor}\n Accuracy: {self.accuracy}\n Dodge: {self.dodge}")
-    
+        print(f"Your character {self.name}'s current stats:\n current Level: {self.level} Health: {self.health}\n Damage: {self.damage}\n Armor: {self.armor}\n Accuracy: {self.accuracy}\n Dodge: {self.dodge}")
+
     def check_inventory(self):
         for item in self.inventory:
             print(item)
 
-    def shoot(self,enemy):
-        for item in self.weapon:   
+    def shoot(self, enemy):
+        for item in self.weapon:
             if 'melee' in item.type:
                 print(f"No melee weapon equipped")
+                return False
             elif 'range' in item.type:
-                total = self.damage + self.weapon[0].damage
-                print(f"You hit the {enemy}. You did {total} damage.")
-    
-    def take_damage(self,damage):
-        total =  damage - self.armor
+                print(f"You hit the {enemy}. You did {self.damage} damage.")
+                return True 
+
+    def take_damage(self, damage):
+        total = damage - self.armor
         self.health -= total
         print(f"You took {total} damage. Remaining health: {self.health}")
 
     def run(self):
-       print(f"You run away from the fight.")
-        
-    """def calc_damage(self):
+        print(f"You run away from the fight.")
+
+    def calc_damage(self):
         total = self.damage + self.weapon[0].damage
-        self.damage = total"""
+        self.damage = total
 
     def hit(self, enemy):
 
@@ -47,7 +49,7 @@ class Character:
                 total = self.damage + self.weapon[0].damage
                 print(f"You hit the {enemy}. You did {total} damage.")
                 return True
-        
+
 
 class Soldier(Character):
     def __init__(self, name):
@@ -61,39 +63,40 @@ class Soldier(Character):
     # def calc_damage(self):
     #     total = self.damage + self.weapon[0].damage
     #     self.damage = total
-    
-    def take_damage(self,damage):
-        total =  damage - self.armor
+
+    def take_damage(self, damage):
+        total = damage - self.armor
         self.health -= total
         print(f"You took {total} damage. Remaining health: {self.health}")
-    
-    def hit(self,enemy):
+
+    def hit(self, enemy):
         super().hit(enemy)
-        
 
     def run(self):
         super().run()
-    
+
     def ammo(self, weapon):
         self.ammo_box -= 1
         print(f"Ammo box used. All weapons reloaded")
+
 
 class Medic(Character):
     def __init__(self, name):
         super().__init__(name, 100, 6, 4)
 
-    def shoot(self,target):
+    def shoot(self, target):
         weapon_dmg = 5
         total = self.damage + self.weapon[0].damage
         print(f"You shoot {target} with your weapon. You do {total} damage")
-    
-    def take_damage(self,damage):
+
+    def take_damage(self, damage):
         super().take_damage(damage)
-        total =  damage - self.armor
+        total = damage - self.armor
         self.health -= total
         self.health += 3
-        print(f"You took {total} damage. You healed yourself for 3 with your self heal ability. Remaining health: {self.health}")
-    
+        print(
+            f"You took {total} damage. You healed yourself for 3 with your self heal ability. Remaining health: {self.health}")
+
     # def calc_damage(self):
     #     total = self.damage + self.weapon[0].damage
     #     self.damage = total
@@ -103,13 +106,13 @@ class Medic(Character):
 
     def run(self):
         super().run()
-    
-    
+
+
 class Demolition(Character):
     def __init__(self, name):
         super().__init__(name, 70, 12, 7)
         self.pipe_bomb = 2
-    
+
     def shoot(self):
         super().shoot()
 
@@ -122,7 +125,8 @@ class Demolition(Character):
     def kaboom(self):
         self.pipe_bomb -= 1
         return f"Thrown pipe bomb! Damaged all enemies for {self.damage * 10} "
-    
+
+
 class Sniper(Character):
     def __init__(self, name):
         super().__init__(name, 60, 15, 3)
@@ -135,10 +139,11 @@ class Sniper(Character):
 
     def run(self):
         super().run()
-    
+
     def sharpshooter(self):
-        
+
         return f"Sharpshooter ability active. You will only deal headshots for the next 10 seconds"
+
 
 class CharacterFactory:
     def create_char(char_class, name):
@@ -154,5 +159,4 @@ class CharacterFactory:
             raise ValueError(f"Invalid character class")
 
 
-
-player = CharacterFactory.create_char("Sniper","Hans")
+player = CharacterFactory.create_char("Sniper", "Hans")
